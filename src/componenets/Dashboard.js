47a01dfcd797d93605192781650ea6908bb4e8ce
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2022-09-01 10:47:05 +0530

    added graphs

--- src/componenets/Dashboard.js
+++ src/componenets/Dashboard.js
@@ -5,6 +5,7 @@ import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import "../css/style.css"
+import BarGraph from './BarGraph';
 function Dashboard() {
 let [dateRange,setDateRange]=useState([{
     startDate: new Date(),
@@ -20,9 +21,23 @@ let handleSelect = (ranges)=>{
       <Navbar navbar="dashboard"/>
 
       <div className="container mx-auto px-4 sm:px-8">
+      <br/>
+        <div className="">
+            <h2 className="text-2xl font-medium leading-tight">Current Sprint</h2><br/>
+            <DateRangePicker
+                ranges={dateRange}
+                onChange={handleSelect}
+            />
+        </div>
+        <div>
+            
+        </div>
+      </div>
+      <BarGraph/>
+      <div className="container mx-auto px-4 sm:px-8">
         <div className="py-8">
             <div className='float-left'>
-                <h2 className="text-2xl font-semibold leading-tight">Release v3.0.15-rc.7</h2>
+                <h2 className="text-2xl font-medium leading-tight">Release v3.0.15-rc.7</h2>
             </div>
             <div className='float-right'>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -32,16 +47,7 @@ let handleSelect = (ranges)=>{
         </div>
       </div>
 
-      <div className="container mx-auto px-4 sm:px-8">
-        <div className="">
-            <p>Current Sprint</p>
-            <DateRangePicker
-                ranges={dateRange}
-                onChange={handleSelect}
-                showDateDisplay={dateRange}
-            />
-        </div>
-      </div>
+      
 
       <Table/>
       
